484d3d51ae16b447367fcaf0b92eb8bd2575ee2b
mrsterner <simon@sterner.dev>
2022-10-26 22:13:56 +0200

    sickle scythe damage

--- src/main/java/dev/sterner/shuckhorror/common/item/SickleItem.java
+++ src/main/java/dev/sterner/shuckhorror/common/item/SickleItem.java
@@ -1,9 +1,12 @@
 package dev.sterner.shuckhorror.common.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterials;
 
-public class SickleItem extends Item {
+public class SickleItem extends SwordItem {
 	public SickleItem(Settings settings) {
-		super(settings);
+		super(ToolMaterials.IRON, 3, -2.4F, settings);
 	}
 }
